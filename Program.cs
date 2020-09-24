9a89ca86906b4ca42ba0d5975dcc169fc5fdf423
HIDDENDEN <me@gmail.com>
2020-09-24 19:36:04 +0300

    Added case of opening all images in directory

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace s02170142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create array of images to work with all of them
+            String[] filePaths = Directory.GetFiles(@".");
+            foreach (string var in filePaths)
+            {
+                Console.WriteLine(var);
+            }
+           
+
+        }
+    }
+}
