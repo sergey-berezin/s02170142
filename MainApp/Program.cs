639b2bad2c5ad65ddc34f035f69a37dda116b4aa
HIDDENDEN <me@gmail.com>
2020-09-30 21:44:04 +0300

    initial commit

---
+++ MainApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+namespace MainApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+            Console.WriteLine("Please enter direcory path you would like to process");
+            Console.WriteLine("Or press enter to use default res folder ");
+            String inputDir = "";
+            inputDir = Console.ReadLine();
+            if (inputDir.Length < 2)
+                inputDir = "/Users/denis/Documents/C#/Lab1WithLibs/ImgProcLib/res";
+
+            ImgProcLib.ImageProcClass imgProc = new ImgProcLib.ImageProcClass(inputDir);
+            // Console.WriteLine(imgProc.GetFilePaths() == null);
+            if (imgProc.GetFilePaths() == null)
+
+                return;
+
+
+            _=Task.Run(() =>
+            {
+                Console.WriteLine("Press Spacebar to cancel");
+                while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar))
+                {
+                }
+                imgProc.InterruptTasks();
+            });
+            await imgProc.StartProc();
+             
+
+
+
+
+        }
+    }
+}
