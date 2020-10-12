c09cf03bfdb2bbed0beffd72be2946a1db16fffb
HIDDENDEN <61973140+HIDDENDEN@users.noreply.github.com>
2020-10-12 11:37:30 +0300

    Task1. V.2_b (#2)
    * initial commit
    
    * Added correct path
    
    * shufflenet_fix
    
    * NEW fixes following stackoverflow
    
    * Refactoring for code beauty
    
    * Now: passing filename into task
    (Was:task index)
    
    * Deleted weird indent
    
    * Replaced'Console.WrieteLine' with 'Trace.WriteLine
    
    * New API instead of string with message
    
    * Added handler for cancelling task at start
    
    * define TRACE
    
    * onnx file add
    
    * _
    
    Co-authored-by: HIDDENDEN <me@gmail.com>

---
+++ MainApp/Program.cs
@@ -0,0 +1,51 @@
+﻿#define TRACE
+using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace MainApp
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+            Console.WriteLine("Please, write direcory path you would like to process");
+            Console.WriteLine("Or press 'ENTER' to use default res folder ");
+            String inputDir = "";
+            inputDir = Console.ReadLine();
+            if (inputDir.Length == 0)
+                //inputDir = "/Users/denis/Documents/C#/Lab1WithLibs/s02170142/ImgProcLib/res";
+                inputDir=@"./ImgProcLib/res";
+            ImgProcLib.ImageProcClass imgProc = new ImgProcLib.ImageProcClass(inputDir);//Create object
+            // Console.WriteLine(imgProc.GetFilePaths() == null);
+            if (imgProc.GetFilePaths() == null)
+                return;
+
+            _=Task.Run(() =>
+            {
+                Console.WriteLine("Press Spacebar to cancel" + '\n');
+                while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar))
+                {
+                }
+                imgProc.InterruptTasks();
+            });
+            await imgProc.StartProc();//Launch Image processing
+             
+
+
+
+
+        }
+    }
+}
