1901b452e48bf9cc45e3a1a699bd9b48dca0874f
HIDDENDEN <me@gmail.com>
2020-11-14 12:38:33 +0300

    Added all needed packages(vers -5.0)

--- AvaloniaLab/ViewModel/ImagesLibraryContext.cs
+++ AvaloniaLab/ViewModel/ImagesLibraryContext.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using ImgProcLib;
+
 namespace ViewModel
 {
     public class ImagesLibraryContext
